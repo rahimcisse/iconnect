57fbe52b89acf1095b9510e47b5e9d5bd0c827eb
rahimcisse <abdulrahimmogtaricisse@gmail.com>
2025-05-19 20:47:36 +0000

    update app.py

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/browse')
+def browse():
+    return render_template('Job-Browse-Page.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
-
+ 
 if __name__ == "__main__":
     app.run(debug=True)
 
