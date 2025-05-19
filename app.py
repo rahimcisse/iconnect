6a22b287329c628eccb90dc23d8d13439a6257ec
rahimcisse <abdulrahimmogtaricisse@gmail.com>
2025-05-19 20:08:28 +0000

    updates

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
